c7182a3969ef2688db0ad1731a114d66e98b82c9
Franco Ezequiel <eze.cavs@hotmail.com>
2022-01-07 17:16:59 -0300

    Tabla de días terminada, falta hacerlo repsonsive, de todos modos fue hecha desde Props

--- src/components/Grafico/Day/Day.js
+++ src/components/Grafico/Day/Day.js
@@ -4,11 +4,11 @@ import './Day.css';
 function Day(props) {
   return (
     <div className='dayContainer'>
-        <p>{props.day}</p>
+        <p className='titleDay'>{props.day}</p>
         <img className='imageDay' src={props.icon} alt="" />
         <div className='temperatureContainer'>
-            <p>{props.maxTemp}</p>
-            <p>{props.minTemp}</p>
+            <p className='temperature maxTemperature'>{props.maxTemp}°C</p>
+            <p className='temperature minTemperature'>{props.minTemp}°C</p>
         </div>
     </div>
   );
