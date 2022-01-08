7522fc13a9c2f77a9d99dc43a64fcad07f7b0f64
Franco Ezequiel <eze.cavs@hotmail.com>
2022-01-07 21:47:35 -0300

    Footer responsive

--- src/components/Grafico/Footer/Footer.js
+++ src/components/Grafico/Footer/Footer.js
@@ -5,7 +5,7 @@ import './Footer.css';
 function Footer() {
   return (
     <footer>
-        <p>Creado por <Link to="//github.com/Franco-Ezequiel-Marchegiani" target="_blank">Franco Ezequiel Marchegiani</Link> - devChallenges.io</p>
+        <p>Creado por <Link className='linkProfileFooter' to="//github.com/Franco-Ezequiel-Marchegiani" target="_blank">Franco Ezequiel Marchegiani</Link> - devChallenges.io</p>
     </footer>
   );
 }
