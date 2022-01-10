4119d0a61a6572b173a4204a9a48f013ac33b0c4
Franco Ezequiel <eze.cavs@hotmail.com>
2022-01-10 16:00:59 -0300

    Encontré la manera de solucionar el switch del sidebar, tengo que añadir otro sidebar abajo y voy switcheando entre un sidebar y el otro, utilizando un state para ello

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,12 +1,10 @@
-import React, {useRef} from 'react';
+import React, {useRef, useState} from 'react';
 import './Sidebar.css';
 import DOMPurify from 'dompurify';
 import IconoEjemplo from '../../Images/Clear.png'
 function SideBar() {
-  const btnSearch = useRef();
-  const placeSearched = () =>{
-    console.log("Hola");
-  }
+  const [sideBar, setSideBar] = useState(false);
+  const showSidebar = () => setSideBar(!sideBar); 
   return (
     /* Usar este video de referencia para lograr el navbar que está oculto
     https://www.youtube.com/watch?v=CXa0f4-dWi4
@@ -14,20 +12,26 @@ function SideBar() {
     https://www.youtube.com/watch?v=Cj7-i-S4TwA
      */
     <section className='sideBarContainer'>
-      <div className='btns-containers'>
-          <button onClick={placeSearched} ref={btnSearch} className='btn-searchPlaces'>Search for places</button>
-          <button className='iconLocation'><i className="fas fa-search-location"></i></button>
-      </div>
-      <div className='iconTimeContainer'>
-      <img className='iconTime' src={IconoEjemplo} alt="" />
-      </div>
-      <h1 className='titleTemperature'><span className='numberTemperature'>15</span><span className='c_temperature'>°c</span></h1>
-      <h2 className='subTitle'>Clear</h2>
-      <p className='textDateToday'>Today <span className='pointSideBar'>•</span> Fri, 5 Jun</p>
-      <div className='containerUbication'>
-      <i className="fas fa-map-marker-alt"></i>
-        <p>Helsinki</p>
-      </div>
+    {/* SideBar visible */}
+      <section className='sideBarDefault'>
+          <div className='btns-containers'>
+              <button onClick={showSidebar} className='btn-searchPlaces'>Search for places</button>
+              <button className='iconLocation'><i className="fas fa-search-location"></i></button>
+          </div>
+          <div className='iconTimeContainer'>
+          <img className='iconTime' src={IconoEjemplo} alt="" />
+          </div>
+          <h1 className='titleTemperature'><span className='numberTemperature'>15</span><span className='c_temperature'>°c</span></h1>
+          <h2 className='subTitle'>Clear</h2>
+          <p className='textDateToday'>Today <span className='pointSideBar'>•</span> Fri, 5 Jun</p>
+          <div className='containerUbication'>
+          <i className="fas fa-map-marker-alt"></i>
+            <p>Helsinki</p>
+          </div> 
+      </section>
+      <section className='sideBarSearch'>
+
+      </section>
     </section>
   );
 }
