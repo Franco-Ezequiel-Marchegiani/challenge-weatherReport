36ec38e0d382460a21c493f7b79d99dc3e983f75
Franco Ezequiel <eze.cavs@hotmail.com>
2022-01-10 20:43:15 -0300

    Mañana queda avanzar con la API, almenos el diseño y estilo ya quedó definido y concluido, queda implementar lógica pura

--- src/components/Grafico/Day/Day.js
+++ src/components/Grafico/Day/Day.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './Day.css';
-
+import Name from '../../../Images/Hail.png' 
 function Day(props) {
+  //{props.icon}
+  // /static/media/Clear.6e4f36b6f663ff2d4d6a.png
+  const nombreImagen = "../../../Images/"
+  const extension = ".png"
   return (
     <div className='dayContainer'>
         <p className='titleDay'>{props.day}</p>
-        <img className='imageDay' src={props.icon} alt="" />
+        <img className='imageDay' src={Name} alt="adsqweqw" />
         <div className='temperatureContainer'>
             <p className='temperature maxTemperature'>{props.maxTemp}°C</p>
             <p className='temperature minTemperature'>{props.minTemp}°C</p>
